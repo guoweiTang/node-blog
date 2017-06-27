212d382abf57a8a8cef130f5a0833a27eac28f92
ice <ice@lagou.com>
2017-06-27 17:22:48 +0800

    个人信息更新（头像、昵称）

--- routes/account.js
+++ routes/account.js
@@ -14,8 +14,24 @@ let storage = multer.diskStorage({
     cb(null, file.fieldname + '-' + Date.now() + file.originalname.substr(file.originalname.lastIndexOf('.')))
   }
 })
-let upload = multer({ storage: storage });
-
+let upload = multer({
+	storage: storage,
+	fileFilter: function (req, file, cb) {
+		let extName = file.originalname.substr(file.originalname.lastIndexOf('.') + 1);
+		if(!isAbleUploadPic(file)){
+			cb(new Error('上传头像只支持扩展名为jpg、png、gif的文件'));
+			return;
+		}
+		cb(null, true);
+	},
+	limits: {
+		fileSize: 1024 * 1024 * 4,
+		files: 1
+	}
+});
+function isAbleUploadPic(file){
+	return (/^image\/(jpeg|gif|png)$/).test(file.mimetype);
+}
 //设置
 router.route('/profile.html')
 .get(function(req, res, next){
@@ -26,35 +42,17 @@ router.route('/profile.html')
 		res.redirect('/login.html');
 	}
 })
-
-router.post('/uploadPicture.json', upload.single('headPic'), function(req, res, next) {
-	console.log(req.file);
-	if(req.file.size > 1024 * 5){
-		res.send({
-			status: -1,
-			message: '上传头像不能超过5MB'
-		})
-		return;
-	}
-	let extName = file.originalname.substr(file.originalname.lastIndexOf('.') + 1);
-	if(!(/^(jpg|png|gif)$/.test(extName))){
-		res.send({
-			status: -1,
-			message: '上传头像只支持扩展名为jpg、png、gif的文件'
-		})
-		return;
-	}
+.post(function(req, res, next){
 	let sessionUser = req.session.user;
-	let serverFileName = req.file.filename;
-	let serverPicturePath = '/upload-sources/i/' + serverFileName;
-	sessionUser.picture = serverPicturePath;
+	sessionUser.picture = req.body.headPic;
 	util.readFileSync('user.json', function(jsonData, fullUrl) {
 		let result = jsonData.result;
 
-		//更新用户头像
+		//更新用户信息
 		for(let user of result){
 			if(user.id === sessionUser.id){
-				user.picture = serverPicturePath;
+				user.name = req.body.user;
+				user.picture = sessionUser.picture;
 				break;
 			}
 		}
@@ -63,14 +61,35 @@ router.post('/uploadPicture.json', upload.single('headPic'), function(req, res, 
 		ws.end(JSON.stringify(jsonData));
 		ws.on('close', function(){
 			res.send({
-				result: {
-					picture: serverPicturePath
-				},
 				status: 1,
 				message: 'success'
 			})
 		})
 	});
+})
+
+router.post('/uploadPicture.json', function(req, res, next) {
+	upload.single('headPic')(req, res, function(err){
+		if(err){
+			if(err.code === 'LIMIT_FILE_SIZE'){
+				err.message = '上传文件不能超过5MB';
+			}
+			res.send({
+				status: -1,
+				message: err.message
+			})
+			return;
+		}
+		let serverFileName = req.file.filename;
+		let serverPicturePath = '/upload-sources/i/' + serverFileName;
+		res.send({
+			result: {
+				picture: serverPicturePath
+			},
+			status: 1,
+			message: 'success'
+		})
+	})
 })
 
 module.exports = router;
